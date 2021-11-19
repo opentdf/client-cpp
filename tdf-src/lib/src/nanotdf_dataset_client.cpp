871b675c6e1a9da7ac37c1a487871cf73abb3c2a
Ben Leggett <bleggett@virtru.com>
2021-11-19 12:20:48 -0500

    Make nanoDataset API use base TDF attribute func

--- tdf-src/lib/src/nanotdf_dataset_client.cpp
+++ tdf-src/lib/src/nanotdf_dataset_client.cpp
@@ -114,7 +114,12 @@ namespace virtru {
 
     /// Allow user to add data attributes
     void NanoTDFDatasetClient::withDataAttributes(const std::vector<std::string>& dataAttributes) {
-        m_nanoTdfClient->withDataAttributes(dataAttributes);
+        //for each URI in vector
+        //TODO should this be extended to take attr friendly descriptions?
+        //TODO should this be extended to take KAS URLs and keys for each attr?
+        for (const auto &attrUri : dataAttributes) {
+            m_nanoTdfClient->withDataAttribute(attrUri, "", "", "");
+        }
     }
 
     ///Allow user to read data attributes associated with this instances of client (to be replaced by inspectDataAttributes)
