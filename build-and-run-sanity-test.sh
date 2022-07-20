44295477d835a030b7337e5c59b84692076b1d0d
sujan kota <sujankota@gmail.com>
2022-07-20 15:13:37 -0400

    Fix the build

--- build-and-run-sanity-test.sh
+++ build-and-run-sanity-test.sh
@@ -2,11 +2,10 @@
 
 echo "Building OpenTDF library and run unittests"
 export VRUN_BACKEND_TESTS="true"
-cd ../../src
-echo "Error: failed to locate tdf library $(pwd)..."
+cd ../../src && ./build-all.sh
 
 echo "Build the sample executable using OpenTDF library"
-cd ../../examples && mkdir build && cd build && cmake .. && make
+cd ../examples && mkdir build && cd build && cmake .. && make
 
 echo "Run the sanity test"
 ./tdf_sample
