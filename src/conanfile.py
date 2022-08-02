93ddcf58a24731ef8fa2f6faa846dd90d413a494
sujan kota <sujankota@gmail.com>
2022-08-02 16:00:11 -0400

    iphone simulator build

--- src/conanfile.py
+++ src/conanfile.py
@@ -4,9 +4,8 @@ class TDFLibConan(ConanFile):
     generators = "cmake"
     settings = "os", "compiler", "build_type", "arch"
 
-    default_options = ("libarchive:with_zlib=False")
-
     def configure(self):
+        self.options["boost"].with_stacktrace_backtrace = False
         if str(self.settings.arch).startswith('arm'):
             self.options["openssl"].no_asm = True
             self.options["libxml2"].zlib = False
@@ -14,6 +13,7 @@ class TDFLibConan(ConanFile):
             self.options["libxml2"].icu = False
         
     def requirements(self):
+        self.requires("libiconv/1.17@")
         self.requires("openssl/1.1.1o@")
         if str(self.settings.arch).startswith('arm'):
             self.requires("boost/1.74.0@")
@@ -22,7 +22,6 @@ class TDFLibConan(ConanFile):
         self.requires("ms-gsl/2.1.0@")
         self.requires("libxml2/2.9.10@")
         self.requires("pybind11/2.6.2@")
-        self.requires("libarchive/3.5.1@")
         self.requires("nlohmann_json/3.10.4@")
         self.requires("jwt-cpp/0.4.0@")
 
