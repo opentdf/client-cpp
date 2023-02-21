73b1a6edf754aac3998df1181ffccc77d1450a3c
sujan kota <sujankota@gmail.com>
2023-02-21 06:23:12 +0530

    PLAT-2340 - OpenTDF C++ SDK - TDF reader and writer interface should be public so wrapper(Virtru) SDK can implement different TDF formats

--- src/lib/src/tdf_archive_writer.h
+++ src/lib/src/tdf_archive_writer.h
@@ -19,6 +19,7 @@
 
 #include "crypto/bytes.h"
 #include "io_provider.h"
+#include "tdf_writer.h"
 #include "logger.h"
 #include "tdf_constants.h"
 #include "zipmanager/zip_headers.h"
@@ -26,30 +27,6 @@
 namespace virtru {
 
     using namespace virtru::crypto;
-
-    class ITDFWriter {
-    public:
-        // Destructor
-        virtual ~ITDFWriter() = default;
-
-        /// Set the payload size of the TDF
-        /// \param payloadSize
-        virtual void setPayloadSize(int64_t payloadSize) = 0;
-
-        /// Append the manifest contents to the archive.
-        /// \param manifest - Contents of the manifest file.
-        /// NOTE: Manifest should be always be added at the end after writing the payload for TDF.
-        /// NOTE: Manifest should be always be added before writing the payload for TDF2.
-        virtual void appendManifest(std::string&& manifest) = 0;
-
-        /// Append the manifest contents to the archive.
-        /// \param payload - encrypted payload.
-        virtual void appendPayload(Bytes payload) = 0;
-
-        /// Finalize archive entry.
-        virtual void finish() = 0;
-    };
-
 
     class TDFArchiveWriter: public ITDFWriter{
     public:
