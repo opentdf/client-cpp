aec95b918ce915f23ddd4c4484e9b9ae8a8614e6
Pat Mancuso <pmancuso@virtru.com>
2023-03-20 15:24:16 -0400

    Populate 'dist' directory on windows

--- src/build-all.bat
+++ src/build-all.bat
@@ -13,6 +13,17 @@ conan build .. --build-folder .
 set builderrorlevel=%errorlevel%
 if %builderrorlevel% neq 0 goto fin
 
+REM Populate dist directory
+cd ..
+dir
+rmdir /s /y ..\dist
+mkdir ..\dist
+xcopy /s build\package\* ..\dist
+xcopy ..\VERSION ..\dist
+xcopy ..\README.md ..\dist
+xcopy ..\LICENSE ..\dist
+xcopy /s ..\examples ..\dist
+
 :fin
 REM return to where we came from
 popd
