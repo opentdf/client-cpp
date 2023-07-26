704d13c56201ea4ae3a7047688d59be6efee44f6
sujan kota <sujankota@gmail.com>
2023-07-26 11:05:24 -0400

    Enable code coverage compilation flag

--- build-and-run-sanity-test.sh
+++ build-and-run-sanity-test.sh
@@ -3,6 +3,7 @@
 echo "Building OpenTDF library and run unittests"
 export VRUN_BACKEND_TESTS="true"
 export VBUILD_UNIT_TESTS="true"
+export VBUILD_CODE_COVERAGE="true"
 cd ../../src && ./build-all.sh
 
 echo "Build the sample executable using OpenTDF library"
