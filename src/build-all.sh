45476824bfc11d8a3c08529a77f3e22c463e0789
sujan kota <sujankota@gmail.com>
2023-07-28 09:57:32 -0400

    Test converage

--- src/build-all.sh
+++ src/build-all.sh
@@ -35,7 +35,7 @@ fi
 
 if [[ "$VBUILD_CODE_COVERAGE" == "true" ]]; then
     echo "Running code coverage..."
-	lcov --capture --directory . --output-file coverage.info
+	lcov --capture --directory src --output-file coverage.info
     genhtml coverage.info --output-directory code-coverage
 	html2text  -width 200 code-coverage/index.html
 	tar -zcvf code-coverage.tar.gz code-coverage
