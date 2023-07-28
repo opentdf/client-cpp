9f87be908cd53794fba68d4802fc2464ce5ca599
sujan kota <sujankota@gmail.com>
2023-07-28 14:41:30 -0400

    Test coverage

--- src/build-all.sh
+++ src/build-all.sh
@@ -40,7 +40,7 @@ if [[ "$VBUILD_CODE_COVERAGE" == "true" ]]; then
     genhtml coverage.info --output-directory code-coverage
 	html2text  -width 200 code-coverage/index.html
 	tar -zcvf code-coverage.tar.gz code-coverage
-    cp  code-coverage.tar.gz ../
+    cp  code-coverage.tar.gz ../../
     echo "Finished coverage."
     ls
 fi
