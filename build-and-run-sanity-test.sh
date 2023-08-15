cf5fcb9993ae1c1bc26f73a5f575331c6ed76567
sievdokymov-virtru <sergii.ievdokymov@virtru.com>
2023-08-15 13:26:39 +0300

    Check coverage data2

--- build-and-run-sanity-test.sh
+++ build-and-run-sanity-test.sh
@@ -4,10 +4,10 @@ echo "Building OpenTDF library and run unittests"
 export VRUN_BACKEND_TESTS="true"
 export VBUILD_UNIT_TESTS="true"
 
-cd src && ./build-all.sh
+cd ../../src && ./build-all.sh
 
 echo "Build the sample executable using OpenTDF library"
-cd examples && mkdir build && cd build && cmake .. && make
+cd ../examples && mkdir build && cd build && cmake .. && make
 
 echo "Run the sanity test"
 ./tdf_sample
