d90b2232b350ee3d9151864ce9bb3f5c25487d67
sievdokymov-virtru <sergii.ievdokymov@virtru.com>
2023-08-22 13:45:15 +0300

    Try to gather gcov files3

--- src/build-all.sh
+++ src/build-all.sh
@@ -53,6 +53,7 @@ if [[ "$VBUILD_CODE_COVERAGE" == "true" ]]; then
     lcov -r coverage_total_step2.info "/home/runner/.conan/data/*" -o coverage_total_final.info
 
     LOCATION=your_gcov_folder_name
+    mkdir $LOCATION
     find -name '*.cpp' -exec cp -t $LOCATION {} +
     find -name '*.gcno' -exec cp -t $LOCATION {} +
     find -name '*.gcda' -exec cp -t $LOCATION {} +
