90356eba12f8f85359553d8a8dedc562a872ddaf
Mikhail Ustyantsev <mustyantsev@lohika.com>
2023-11-03 17:14:19 +0200

    use Gihub Bot instead PAT

--- .github/workflows/create_auto_pr.sh
+++ .github/workflows/create_auto_pr.sh
@@ -15,7 +15,6 @@ branch_name="automatic-update-to-$LATEST_VERSION"
 cd wrapper_repo
 ls -al
 cat .git/config
-git remote set-url --push origin git@github.com:$REPO_NAME.git
 
 git checkout -b $branch_name
 
