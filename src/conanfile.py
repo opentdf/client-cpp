bef12d965c01fa04e9fc736d05c642392b753824
Pat Mancuso <pmancuso@virtru.com>
2024-06-12 15:54:23 -0400

    bump versions

--- src/conanfile.py
+++ src/conanfile.py
@@ -19,11 +19,11 @@ class TDFLibConan(ConanFile):
         
     def requirements(self):
         self.requires("openssl/[>=3.1 <4]")
-        self.requires("boost/1.83.0")
+        self.requires("boost/1.85.0")
         self.requires("ms-gsl/2.1.0")
-        self.requires("libxml2/2.11.6")
+        self.requires("libxml2/2.12.7")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("jwt-cpp/0.6.0")
+        self.requires("jwt-cpp/0.7.0")
         self.requires("zlib/[>=1.2.11 <2]")
         self.requires("magic_enum/0.8.2")
         self.requires("picojson/cci.20210117")
